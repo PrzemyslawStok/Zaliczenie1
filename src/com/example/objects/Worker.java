82eaa2b372fbafde6407363e0120fd63f9f0a3ff
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-02-29 12:23:48 +0100

    commit 3

--- src/com/example/objects/Worker.java
+++ src/com/example/objects/Worker.java
@@ -2,6 +2,7 @@ package com.example.objects;
 
 public class Worker {
     String name;
+    String surname;
 
     public void setName(String name) {
         this.name = name;
@@ -9,5 +10,13 @@ public class Worker {
 
     public String getName() {
         return name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getSurname() {
+        return surname;
     }
 }
